66bb2c10226c61d7bef15f9969a907aab818d615
abx903 <Philipp.Goemann@haw-hamburg.de>
2017-11-24 20:25:01 +0900

    Added openApp() function, goToMarket(), isSkypeClientInstalled()

--- Helper/app/src/main/java/kr/kaist/helper/AppList.java
+++ Helper/app/src/main/java/kr/kaist/helper/AppList.java
@@ -1,13 +1,16 @@
 package kr.kaist.helper;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.HashMap;
 
 public class AppList extends AppCompatActivity {
 
@@ -49,7 +52,50 @@ public class AppList extends AppCompatActivity {
     }
 
     public boolean OpenApp(String appName) {
-        //TODO
+        /**
+         * Opens the Skype app if it is installed,
+         * refers to the market to download it otherwise
+         */
+        //Note: I am not sure about the Context thing yet, and whether one has to pass a Context.
+        //So far we would not need a HashMap since we only have 1 app, but this makes it easier to change it lateron
+        HashMap<String,String> packageMap = new HashMap<>();
+        packageMap.put("Skype","com.skype.raider");
+        if (isSkypeClientInstalled()) {
+
+            PackageManager packageManager = getPackageManager();
+            Intent skypeIntent = packageManager.getLaunchIntentForPackage(packageMap.get(appName));
+            startActivity(skypeIntent);
+
+        } else {
+            goToMarket();
+        }
         return true;
     }
+
+    /**
+     * Install the Skype client through the market: URI scheme.
+     */
+    public void goToMarket() {
+        Uri marketUri = Uri.parse("market://details?id=com.skype.raider");
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, marketUri);
+        myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(myIntent);
+
+        return;
+    }
+
+    /**
+     * Determine whether the Skype for Android client is installed on this device.
+     */
+    public boolean isSkypeClientInstalled() {
+        PackageManager myPackageMgr = getPackageManager();
+        try {
+            myPackageMgr.getPackageInfo("com.skype.raider", PackageManager.GET_ACTIVITIES);
+        } catch (PackageManager.NameNotFoundException e) {
+            return (false);
+        }
+        return (true);
+    }
+
+
 }
