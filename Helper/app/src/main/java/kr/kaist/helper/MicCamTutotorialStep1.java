1d58c6d023194da641426c6dffcef65051836dad
abx903 <Philipp.Goemann@haw-hamburg.de>
2017-11-24 22:11:18 +0900

    Added templates for the MicCamTutorial

---
+++ Helper/app/src/main/java/kr/kaist/helper/MicCamTutotorialStep1.java
@@ -0,0 +1,13 @@
+package kr.kaist.helper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MicCamTutotorialStep1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mic_cam_tutotorial_step1);
+    }
+}
