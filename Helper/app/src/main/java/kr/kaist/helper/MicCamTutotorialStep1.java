785cdd0e4d1bbca7f2bb4cf60a2d4e2e5b64302f
abx903 <Philipp.Goemann@haw-hamburg.de>
2017-11-26 20:05:46 +0900

    current status, cant find the bug

--- Helper/app/src/main/java/kr/kaist/helper/MicCamTutotorialStep1.java
+++ Helper/app/src/main/java/kr/kaist/helper/MicCamTutotorialStep1.java
@@ -2,6 +2,7 @@ package kr.kaist.helper;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -9,8 +10,8 @@ import android.widget.TextView;
 public class MicCamTutotorialStep1 extends AppCompatActivity {
 
     private int step;
-    private TextView stepText;
-    private TextView instructionText;
+    private TextView progressText;
+    private TextView taskText;
     private ImageView instructionImage;
     private Button startButton;
 
@@ -20,13 +21,29 @@ public class MicCamTutotorialStep1 extends AppCompatActivity {
         setContentView(R.layout.activity_mic_cam_tutotorial_step1);
 
         step = 1;
-        stepText =  findViewById(R.id.stepText);
-        instructionText =  findViewById(R.id.instructionText);
+        progressText =  findViewById(R.id.progressText);
+        taskText =  findViewById(R.id.taskText);
         instructionImage=  findViewById(R.id.instructionImage);
+        startButton = findViewById(R.id.buttonStart);
 
-        instructionImage.setImageResource(R.drawable.startscreencammic);
-        stepText.setText("Welcome! In this Tutorial we show you how to disable/enable your camera and microphone");
-        instructionText.setText("Right now, your microphone is enabled and your camera disabled. If you are ready, press the Button");
 
+
+        instructionImage.setImageResource(R.drawable.introcammic);
+        progressText.setText("Welcome! In this Tutorial we show you how to disable/enable your camera and microphone");
+        taskText.setText("Right now, your microphone is enabled and your camera disabled. If you are ready, press the Button");
+
+        View.OnClickListener buttonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToStep2();
+            }
+        };
+        startButton.setOnClickListener(buttonListener);
+
+
+    }
+
+    private void goToStep2() {
+        
     }
 }
