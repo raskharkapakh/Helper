33749a5b2c14f27a8b9da9b386972f186e7688cf
abx903 <Philipp.Goemann@haw-hamburg.de>
2017-11-26 22:34:21 +0900

    Tutorial finished, only final picture/Step 3 picture missing

--- Helper/app/src/main/java/kr/kaist/helper/MicCamTutotorialStep1.java
+++ Helper/app/src/main/java/kr/kaist/helper/MicCamTutotorialStep1.java
@@ -1,5 +1,6 @@
 package kr.kaist.helper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,6 +15,8 @@ public class MicCamTutotorialStep1 extends AppCompatActivity {
     private TextView taskText;
     private ImageView instructionImage;
     private Button startButton;
+    private Button hiddenMicButton;
+    private Button hiddenCamButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +28,25 @@ public class MicCamTutotorialStep1 extends AppCompatActivity {
         taskText =  findViewById(R.id.taskText);
         instructionImage=  findViewById(R.id.instructionImage);
         startButton = findViewById(R.id.buttonStart);
+        hiddenMicButton = findViewById(R.id.MicButton);
+        hiddenCamButton = findViewById(R.id.CamButton);
 
 
 
-        instructionImage.setImageResource(R.drawable.introcammic);
+
+        instructionImage.setImageResource(R.drawable.startscreencammicscaled);
         progressText.setText("Welcome! In this Tutorial we show you how to disable/enable your camera and microphone");
         taskText.setText("Right now, your microphone is enabled and your camera disabled. If you are ready, press the Button");
 
         View.OnClickListener buttonListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(step==4){
+                    Intent intent = new Intent(MicCamTutotorialStep1.this, SkypeTutorial.class);
+                    startActivity(intent);
+                }else{
                 goToStep2();
+                }
             }
         };
         startButton.setOnClickListener(buttonListener);
@@ -44,6 +55,49 @@ public class MicCamTutotorialStep1 extends AppCompatActivity {
     }
 
     private void goToStep2() {
-        
+        /*ViewGroup layout = (ViewGroup) startButton.getParent();
+        if(null!=layout) //for safety only  as you are doing onClick
+            layout.removeView(startButton);*/
+        startButton.setVisibility(View.GONE);
+        startButton.setClickable(false);
+        instructionImage.setImageResource(R.drawable.mutemictaskscaled);
+        progressText.setText("Step 1");
+        taskText.setText("Click the microhpone button to disable it.");
+        View.OnClickListener micButtonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToStep3();
+            }
+        };
+        hiddenMicButton.setOnClickListener(micButtonListener);
+
+    }
+
+    private void goToStep3() {
+        hiddenMicButton.setClickable(false);
+        //chang instructionImage Pic
+
+        progressText.setText("Step 2");
+        taskText.setText("Well done! Now click the camera button to enable it.");
+        View.OnClickListener camButtonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToStep4();
+            }
+        };
+        hiddenCamButton.setOnClickListener(camButtonListener);
+    }
+
+    private void goToStep4() {
+        step = 4;
+        hiddenCamButton.setClickable(false);
+        startButton.setVisibility(View.VISIBLE);
+        startButton.setClickable(true);
+        instructionImage.setImageResource(R.drawable.done);
+        progressText.setText("Congratulations! You are done!");
+        taskText.setText("Click the Button to return to the menu");
+        startButton.setText("Menu");
+
+
     }
 }
